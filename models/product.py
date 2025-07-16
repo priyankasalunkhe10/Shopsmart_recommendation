6ed433fe01887ba2e06aac35b5c4dbd246acbbc3
unknown <salunkhepriyanka6542@gmail.com>
2025-07-16 15:07:37 +0530

    updated

--- models/product.py
+++ models/product.py
@@ -5,6 +5,6 @@ class Product(Base):
     __tablename__ = 'products'
 
     product_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    description = Column(String)
+    name = Column(String(100))
+    description = Column(String(100))
     price = Column(Float)
