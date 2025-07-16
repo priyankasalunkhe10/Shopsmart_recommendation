bdde271ce46527c8526add6de921035a6bb44396
unknown <salunkhepriyanka6542@gmail.com>
2025-07-16 15:08:41 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from database import Base,engine
+from routes import customer_routes
+import models.customer
+import models.product
+import models.transaction
+
+app=Flask(__name__)
+
+Base.metadata.create_all(bind=engine)
+
+app.register_blueprint(customer_routes.router)
+
+@app.route('/')
+def home():
+    return "Shopsmart API is running"
+
+if __name__=='__main__':
+    app.run(debug=True)
